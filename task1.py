6958c499b4749ec19460309b13e11356d6973c36
varunsharma2447GenAi <varunsharma2447@gmail.com>
2026-02-10 16:43:43 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+num1=int(input ('Enter a Number: '))
+num2=int(input ('Enter another Number: '))
+a=num1+num2
+s=num1-num2
+m=num1*num2
+d=num1/num2
+print(a)
+print(s)
+print(m)
+print(d)
---
+++ task2.py
@@ -0,0 +1,4 @@
+firstname=input("Enter your first name: ")
+lastname=input("Enter your last name: ")
+
+print('Hello',firstname,lastname,'! welocme to the Python program.')
